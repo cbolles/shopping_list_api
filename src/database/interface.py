3c57710ac1d687c801c81138d22fa790d63c6e45
Collin Bolles <collinbolles@gmail.com>
2019-02-17 04:21:17 -0500

    Add ability to retrieve the list from the database

--- src/database/interface.py
+++ src/database/interface.py
@@ -25,3 +25,16 @@ def save_item(item):
     client = get_db()
     items = client.get_collection(get_config()['database']['collection_items'])
     items.insert_one(item.as_dict())
+
+
+def get_list():
+    client = get_db()
+    items = client.get_collection(get_config()['database']['collection_items'])
+    all_items = items.find()
+    item_response = []
+    for item in all_items:
+        item_dict = dict()
+        item_dict['product_sku'] = item['product_sku']
+        item_dict['product_name'] = item['product_name']
+        item_response.append(item_dict)
+    return item_response
